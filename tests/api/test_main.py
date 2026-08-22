42d664b74a25132a480520f2b80ef7cdfa22be55
alexandrebsc <alexandrebscoelho@gmail.com>
2026-08-22 00:04:58 -0300

    refactor api tests | add train tests

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -1,18 +1,19 @@
-"""Teste """
+"""Testes relativos à src/api/main."""
 from fastapi.testclient import TestClient
 
 from src.api.main import app
 
 client = TestClient(app)
 
+STATUS_CODE_OK = 200
 
-def test_health():
+def test_health() -> None:
     response = client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code == STATUS_CODE_OK
     assert response.json() == {"status": "API is online"}
 
 
-def test_predict_churn_valido():
+def test_predict() -> None:
     payload = {
         "gender": "Female",
         "SeniorCitizen": 0,
@@ -37,7 +38,7 @@ def test_predict_churn_valido():
 
     response = client.post("/predict", json=payload)
 
-    assert response.status_code == 200
+    assert response.status_code == STATUS_CODE_OK
 
     data = response.json()
     assert "churn_prediction" in data
